65df8f5b5d2fca4658cc4a8556c2b62646b8e139
ANKIT KUMAR <ankitkumarjl361@gmail.com>
2024-09-26 11:45:06 +0530

    vite.config.js file changed

--- client/src/http/networkRequest.js
+++ client/src/http/networkRequest.js
@@ -1,21 +1,21 @@
 import axios from "axios";
-const api = axios.create({
-  baseURL: "http://localhost:8000",
-  withCredentials: true,
-});
-export const userRegister = (data) => api.post("/api/user/register", data);
-export const userLogin = (data) => api.post("/api/user/login", data);
-export const logoutuser = () => api.get("/api/user/logout");
+// const api = axios.create({
+//   baseURL: "http://localhost:8000",
+//   withCredentials: true,
+// });
+export const userRegister = (data) => axios.post("/api/user/register", data);
+export const userLogin = (data) => axios.post("/api/user/login", data);
+export const logoutuser = () => axios.get("/api/user/logout");
 export const updateprofile = (userId, data) =>
-  api.put(`/api/user/updateprofile/${userId}`, data);
+  axios.put(`/api/user/updateprofile/${userId}`, data);
 export const currentpwdstatus = (userId, data) =>
-  api.post(`/api/user/currentpwdstatus/${userId}`, data);
+  axios.post(`/api/user/currentpwdstatus/${userId}`, data);
 export const addProduct = (userId, data) =>
-  api.post(`/api/product/addproduct/${userId}`, data);
+  axios.post(`/api/product/addproduct/${userId}`, data);
 export const getProducts = () => api.get("/api/product/getproducts");
 export const singleProduct = (productId) =>
-  api.get(`/api/product/singleproduct/${productId}`);
+  axios.get(`/api/product/singleproduct/${productId}`);
 export const similarProductsFromServer = (queryParams) =>
-  api.get(`/api/product/similarproducts?${queryParams}`);
+  axios.get(`/api/product/similarproducts?${queryParams}`);
 export const updateProduct = (userId, productId, formData) =>
-  api.post(`/api/product/updateproduct/${userId}/${productId}`, formData);
+  axios.post(`/api/product/updateproduct/${userId}/${productId}`, formData);
